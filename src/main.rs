f4008775db546ee66a6c3f0216df3aac3b7639b6
Aympab <aymeric.millan@mines-paristech.fr>
2022-03-24 16:49:32 +0100

    bevyengine tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,32 @@
-// mod color;
+use bevy::prelude::*;
 
-// fn main() {
-//     println!("Hello world !");
-//     let blue = color::RGBColor { r:0, g:0, b:255};
-//     // let red = color::Color(255,0,0);
+fn hello_world() {
+    println!("hello world!");
+}
 
-//     println!("{}, {}, {}", blue.r, blue.g, blue.b);
-//     // println!("{}", red.0);
-// }
+#[derive(Component)]
+struct Person;
 
-// #[test]
-// fn should_pass(){
-//     assert!(1 == 1);
-// }
+#[derive(Component)]
+struct Name(String);
 
-// #[test]
-// fn should_fail(){
-//     unimplemented!();
-// }
-use rapier3d::prelude::*;
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
 
 fn main() {
-  let mut rigid_body_set = RigidBodySet::new();
-  let mut collider_set = ColliderSet::new();
-
-  /* Create the ground. */
-  let collider = ColliderBuilder::cuboid(100.0, 0.1, 100.0).build();
-  collider_set.insert(collider);
-
-  /* Create the bounding ball. */
-  let rigid_body = RigidBodyBuilder::new_dynamic()
-          .translation(vector![0.0, 10.0, 0.0])
-          .build();
-  let collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
-  let ball_body_handle = rigid_body_set.insert(rigid_body);
-  collider_set.insert_with_parent(collider, ball_body_handle, &mut rigid_body_set);
-
-  /* Create other structures necessary for the simulation. */
-  let gravity = vector![0.0, -9.81, 0.0];
-  let integration_parameters = IntegrationParameters::default();
-  let mut physics_pipeline = PhysicsPipeline::new();
-  let mut island_manager = IslandManager::new();
-  let mut broad_phase = BroadPhase::new();
-  let mut narrow_phase = NarrowPhase::new();
-  let mut joint_set = JointSet::new();
-  let mut ccd_solver = CCDSolver::new();
-  let physics_hooks = ();
-  let event_handler = ();
-
-  /* Run the game loop, stepping the simulation once per frame. */
-  for _ in 0..200 {
-    physics_pipeline.step(
-      &gravity,
-      &integration_parameters,
-      &mut island_manager,
-      &mut broad_phase,
-      &mut narrow_phase,
-      &mut rigid_body_set,
-      &mut collider_set,
-      &mut joint_set,
-      &mut ccd_solver,
-      &physics_hooks,
-      &event_handler,
-    );
-
-    let ball_body = &rigid_body_set[ball_body_handle];
-    println!(
-      "Ball altitude: {}",
-      ball_body.translation().y
-    );
-  }
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
 }
