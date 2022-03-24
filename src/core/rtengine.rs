f7fadb807fdee1792f3045051c1ff7ff5ad09723
Aympab <aymeric.millan@mines-paristech.fr>
2022-03-24 22:27:20 +0100

    implemented tests

--- src/core/rtengine.rs
+++ src/core/rtengine.rs
@@ -11,18 +11,23 @@ impl RTEngine {
     }
 }
 
+#[cfg(test)]
+mod rtengine_tests{
+    use crate::utils::geometry::Point;
+    use crate::core::rtengine::RTEngine;
+    
+    #[test]
+    fn basic_instanciation() {
+        let origin = Point{ ..Default::default() };
 
-#[test]
-fn default_instanciation() {
-    let origin = Point{ ..Default::default() };
+        let origins = Vec::from([origin, origin, origin]);
 
-    let origins = Vec::from([origin, origin, origin]);
+        let rte = RTEngine{
+            p_camera : origin,
+            p_pixels : origins
+        };
 
-    let rte = RTEngine{
-        p_camera : origin,
-        p_pixels : origins
-    };
-
-    assert_eq!(rte.p_camera, origin);
-    assert_eq!(rte.p_pixels.len(), 3);
+        assert_eq!(rte.p_camera, origin);
+        assert_eq!(rte.p_pixels.len(), 3);
+    }
 }
--- src/utils/geometry.rs
+++ src/utils/geometry.rs
@@ -32,3 +32,32 @@ impl Add for Point {
         }
     }
 }
+
+
+#[cfg(test)]
+mod geometry_tests{
+    use crate::utils::geometry::Point;
+
+    #[test]
+    fn default_instanciation() {
+        let p = Point{..Default::default()};
+
+        assert_eq!(p.x, 0.0f32);
+        assert_eq!(p.y, 0.0f32);
+        assert_eq!(p.z, 0.0f32);
+    }
+
+    #[test]
+    fn simple_add() {
+        let offset = 10f32;
+
+        let p1 = Point{..Default::default()};
+        let p2 = Point{x : offset, y : offset, z : offset};
+
+        let p = p1 + p2;
+
+        assert_eq!(p.x, p1.x + p2.x);
+        assert_eq!(p.y, p1.y + p2.y);
+        assert_eq!(p.z, p1.z + p2.z);
+    }
+}
