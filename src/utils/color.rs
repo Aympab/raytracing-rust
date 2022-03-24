d133d8234fbc281b1ffd8a575a7ddace5c0d2191
Aympab <aymeric.millan@mines-paristech.fr>
2022-03-24 22:53:18 +0100

    generated doc

--- src/utils/color.rs
+++ src/utils/color.rs
@@ -1,3 +1,4 @@
+/// A basic RGB color with unsigned 8 bits integers (0..255)
 pub struct RGBColor {
     pub r : u8,
     pub g : u8,
--- src/utils/geometry.rs
+++ src/utils/geometry.rs
@@ -1,18 +1,20 @@
-// pub mod geometry;
 use std::ops::Add;
 
 // #[derive(Default)]
 #[derive(Debug, Copy, Clone)]
+/// Point represents a basic 3D point in space
 pub struct Point {
     pub x : f32,
     pub y : f32,
     pub z : f32,
 }
 
+/// By default, the position of a point is the origin : 0,0,0
 impl Default for Point {
     fn default() -> Self { Point { x: 0.0f32, y: 0.0f32, z: 0.0f32 }}
 }
 
+/// Point == Point if x, y and z are equals
 impl PartialEq for Point {
     fn eq(&self, other: &Self) -> bool {
         return &self.x == &other.x
@@ -21,6 +23,7 @@ impl PartialEq for Point {
     }
 }
 
+/// Adding two points is adding all of their components
 impl Add for Point {
     type Output = Point;
 
@@ -59,5 +62,34 @@ mod geometry_tests{
         assert_eq!(p.x, p1.x + p2.x);
         assert_eq!(p.y, p1.y + p2.y);
         assert_eq!(p.z, p1.z + p2.z);
+    }
+
+    #[test]
+    fn simple_eq() {
+        let v = 10f32;
+
+        let p1 = Point{x : v, y : v, z : v};
+        let p2 = Point{x : v, y : v, z : v};
+
+        assert_eq!(p1, p2);
+    }
+
+    #[test]
+    fn various_operator() {
+        let mut p1 = Point{..Default::default()};
+        let mut sum : f32 = 0f32;
+
+        for i in 0..100 {
+            let n = i as f32; 
+            p1.x += n;
+            p1.y += n;
+            p1.z += n;
+
+            sum += n;
+        }
+
+        let p2 = Point{ x : sum, y : sum, z : sum};
+        assert_eq!(p1, p2);
+        assert_eq!(p1 + p2, p1 + p1);
     }
 }
