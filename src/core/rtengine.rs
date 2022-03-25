5c5ce68fad99659aee0b21f22ac77396545e20bc
Aympab <aymeric.millan@mines-paristech.fr>
2022-03-25 13:27:03 +0100

    changed types from vector to Array2 from ndarray

--- src/core/rtengine.rs
+++ src/core/rtengine.rs
@@ -2,16 +2,22 @@ use crate::utils::color::RGBColor;
 use crate::utils::geometry::Point;
 use crate::utils::geometry::Line;
 
+use ndarray::Array2;
+
 const MAX_DEPTH: i32 = 10;
 
 pub struct RTEngine{
     pub pos_camera : Point,
-    pub pos_pixels : Vec<Point>,
+    pub pos_pixels : Array2::<Point>,
+    // pub pos_pixels : Vec<Point>,
 }
 
 impl RTEngine {
     ///The simplest ray tracing algorithm : path tracing
     pub fn path_tracing(&mut self) -> Vec<RGBColor> {
+
+        // let a = Array2::<RGBColor>::zeros(shape) s
+
         let mut colors = Vec::with_capacity(self.pos_pixels.len());
 
         for p in &self.pos_pixels {
