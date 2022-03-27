b4c63e6abee13ca7b61021fb37adc580bb797965
Aympab <aymeric.millan@mines-paristech.fr>
2022-03-27 15:28:56 +0200

    trying to create a sphere in another file

--- src/bin.rs
+++ src/bin.rs
@@ -1,18 +1,20 @@
+pub mod utils;
+pub mod bevy_utils;
+
 use bevy::prelude::*;
-use bevy_mod_picking::{
-    DebugCursorPickingPlugin, DebugEventsPickingPlugin, DefaultPickingPlugins, PickableBundle,
-    PickingCameraBundle,
-};
+use bevy_utils::mesh::create_sphere;
+use utils::geometry::Point;
+
 
 fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
+            title: "Raytracing app".to_string(),
+            width : 500.0,
+            height : 300.0,
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
-        .add_plugins(DefaultPickingPlugins) // <- Adds Picking, Interaction, and Highlighting plugins.
-        .add_plugin(DebugCursorPickingPlugin) // <- Adds the green debug cursor.
-        .add_plugin(DebugEventsPickingPlugin) // <- Adds debug event logging.
         .add_startup_system(setup)
         .run();
 }
@@ -29,19 +31,31 @@ fn setup(
             mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
             material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
             ..Default::default()
-        })
-        .insert_bundle(PickableBundle::default()); // <- Makes the mesh pickable.
+        });
+        // .insert_bundle(PickableBundle::default()); // <- Makes the mesh pickable.
                                                    // cube
-    commands
+
+        ///3 spheres
+        let c = Point{..Default::default()};
+        commands.spawn_bundle(create_sphere(&c, meshes.copy(), materials));
+        
+        commands
         .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            mesh: meshes.add(Mesh::from(shape::UVSphere { ..Default::default() })),
+            material: materials.add(Color::rgb(0.2, 0.4, 0.6).into()),
+            transform: Transform::from_xyz(2.0, 0.6, -2.0),
             ..Default::default()
-        })
-        .insert_bundle(PickableBundle::default()); // <- Makes the mesh pickable.
-                                                   // light
-    commands.spawn_bundle(PointLightBundle {
+        });
+
+        commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::UVSphere { ..Default::default() })),
+            material: materials.add(Color::rgb(0.2, 0.4, 0.6).into()),
+            transform: Transform::from_xyz(2.0, 0.6, -2.0),
+            ..Default::default()
+        });
+
+        commands.spawn_bundle(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
             shadows_enabled: true,
@@ -50,11 +64,12 @@ fn setup(
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..Default::default()
     });
+
     // camera
     commands
         .spawn_bundle(PerspectiveCameraBundle {
             transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
-        })
-        .insert_bundle(PickingCameraBundle::default()); // <- Sets the camera to use for picking.
+        });
+        // .insert_bundle(PickingCameraBundle::default()); // <- Sets the camera to use for picking.
 }
