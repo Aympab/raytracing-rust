67275baf243756da6ed18897ec643deb2030fe4b
bourbonut <ben.bourbon06@gmail.com>
2022-04-10 18:08:25 +0200

    Implementation of main function for raytracing (not yet tested)

--- src/core/rtengine.rs
+++ src/core/rtengine.rs
@@ -1,16 +1,22 @@
-use crate::utils::color::RGBColor;
-use crate::utils::geometry::Line;
+// use crate::utils::color::RGBColor;
+// use crate::utils::geometry::Line;
 // use crate::utils::geometry::Point;
 
 use glam::Vec3A;
 use ndarray::Array2;
 
-const MAX_DEPTH: i32 = 10;
+// Avoid recurrent algorithm
+// const MAX_DEPTH: i32 = 10;
+const AMBIANT_LIGHT: Vec3A = Vec3A::new(1., 1., 1.);
+const DIFFUSE_LIGHT: Vec3A = Vec3A::new(1., 1., 1.);
+const SPECULAR_LIGHT: Vec3A = Vec3A::new(1., 1., 1.);
 
 pub struct RTEngine {
     pub pos_camera: Vec3A,
     pub pos_pixels: Array2<Vec3A>,
-    // pub pos_pixels : Vec<Vec3A>,
+    pub pos_light: Vec3A,
+    pub objects: Vec<Sphere>,
+    pub material: Vec<Material>,
 }
 
 #[derive(Default, Copy, Clone)]
@@ -19,59 +25,110 @@ pub struct Sphere {
     pub radius: f32,
 }
 
+pub struct Material {
+    pub ambiant: Vec3A,
+    pub diffuse: Vec3A,
+    pub specular: Vec3A,
+    pub shininess: f32,
+    pub reflection: f32,
+}
+
 impl RTEngine {
-    ///The simplest ray tracing algorithm : path tracing
-    pub fn path_tracing(&mut self) -> Array2<RGBColor> {
+    // The simplest ray tracing algorithm : path tracing
+    pub fn path_tracing(&mut self) -> Array2<Vec3A> {
         let width: usize = self.pos_pixels.shape()[0];
-        let hieght: usize = self.pos_pixels.shape()[1];
+        let height: usize = self.pos_pixels.shape()[1];
 
-        let mut colors = Array2::<RGBColor>::default((width, hieght));
+        let mut colors = Array2::<Vec3A>::default((width, height));
 
-        for ((i, j), p) in self.pos_pixels.indexed_iter() {
-            let ray = Line {
-                p1: self.pos_camera,
-                p2: *p,
-            };
-
-            colors[[i, j]] = color_contribution(ray, 0);
+        for ((i, j), pixel) in self.pos_pixels.indexed_iter() {
+            colors[[i, j]] = self.color_contribution(pixel, 0);
         }
 
         return colors;
     }
-}
 
-fn color_contribution(ray: Line, depth: i32) -> RGBColor {
-    let color = RGBColor {
-        ..Default::default()
-    }; //TODO : set background color
+    fn color_contribution(&mut self, pixel: &Vec3A, max_depth: i32) -> Vec3A {
+        let mut origin = self.pos_camera;
+        let mut direction: Vec3A = (*pixel - origin).normalize();
+        // let color = RGBColor {
+        //     ..Default::default()
+        // }; // TODO : set background color
 
-    if depth > MAX_DEPTH {
-        return color;
-    };
-    // let _p = intersect(ray);
-    // if &p == std::ptr::null { return color};
-    //TODO:
-    return color;
-}
+        let mut color = Vec3A::new(0., 0., 0.);
 
-fn nearest_intersected_object(
-    objects: &Vec<Sphere>,
-    ray_origin: Vec3A,
-    ray_direction: Vec3A,
-) -> (Sphere, f32) {
-    let mut distances = Vec::with_capacity(objects.len());
-    for (i, obj) in objects.iter().enumerate() {
-        distances[i] = sphere_intersect(obj.center, obj.radius, ray_origin, ray_direction);
-    }
-    let mut nearest_object: Sphere = Sphere::default();
-    let mut min_distance: f32 = std::f32::INFINITY;
-    for (index, distance) in distances.iter().enumerate() {
-        if distance.is_sign_positive() && distance < &min_distance {
-            min_distance = *distance;
-            nearest_object = objects[index];
+        let mut reflection = 1.;
+
+        for k in 0..max_depth {
+            let (target_index, min_distance): (i32, f32) =
+                self.nearest_intersected_object(origin, direction);
+            if !target_index.is_positive() {
+                break;
+            }
+            // Object and material given the ray
+            let nearest_object: Sphere = self.objects[target_index as usize];
+            let material: Material = self.material[target_index as usize];
+
+            // Intersection computation
+            let intersection = origin + min_distance * direction;
+            let normal_to_surface = (intersection - nearest_object.center).normalize();
+            let shifted_point = intersection + 1e-5 * normal_to_surface;
+            let intersection_to_light = (self.pos_light - shifted_point).normalize();
+
+            let (_, min_distance): (i32, f32) =
+                self.nearest_intersected_object(shifted_point, intersection_to_light);
+            let intersection_to_light_distance = (self.pos_light - intersection).length();
+            if min_distance < intersection_to_light_distance {
+                break;
+            }
+
+            let mut illumination: Vec3A = Vec3A::new(0., 0., 0.);
+
+            // Ambiant contribution
+            illumination += material.ambiant * AMBIANT_LIGHT;
+
+            // Diffuse contribution
+            illumination +=
+                material.diffuse * DIFFUSE_LIGHT * intersection_to_light.dot(normal_to_surface);
+
+            // Specular contribution
+            let intersection_to_camera = (self.pos_camera - intersection).normalize();
+            let h = (intersection_to_light + intersection_to_camera).normalize();
+            illumination += material.specular
+                * SPECULAR_LIGHT
+                * normal_to_surface.dot(h).powf(material.shininess * 0.25);
+
+            // Reflection
+            color += reflection * illumination;
+            reflection *= material.reflection;
+
+            // New origin and direction
+            origin = shifted_point.clone();
+            direction = reflected(direction, normal_to_surface);
         }
+
+        return clip(color, 0., 1.);
     }
-    (nearest_object, min_distance)
+
+    fn nearest_intersected_object(
+        &mut self,
+        ray_origin: Vec3A,
+        ray_direction: Vec3A,
+    ) -> (i32, f32) {
+        let mut distances = Vec::with_capacity(self.objects.len());
+        for (i, obj) in self.objects.iter().enumerate() {
+            distances[i] = sphere_intersect(obj.center, obj.radius, ray_origin, ray_direction);
+        }
+        let mut nearest_object: i32 = -1;
+        let mut min_distance: f32 = std::f32::INFINITY;
+        for (index, distance) in distances.iter().enumerate() {
+            if distance.is_sign_positive() && distance < &min_distance {
+                min_distance = *distance;
+                nearest_object = index as i32;
+            }
+        }
+        (nearest_object, min_distance)
+    }
 }
 
 fn sphere_intersect(center: Vec3A, radius: f32, ray_origin: Vec3A, ray_direction: Vec3A) -> f32 {
@@ -86,4 +143,13 @@ fn sphere_intersect(center: Vec3A, radius: f32, ray_origin: Vec3A, ray_direction
         result = t1.min(t2);
     }
     result
+}
+
+fn reflected(vector: Vec3A, axis: Vec3A) -> Vec3A {
+    vector - 2. * vector.dot(axis) * axis
+}
+
+fn clip(vector: Vec3A, a_min: f32, a_max: f32) -> Vec3A {
+    let one: Vec3A = Vec3A::new(1., 1., 1.);
+    vector.min(one * a_max).max(one * a_min)
 }
