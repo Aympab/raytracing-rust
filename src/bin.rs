dccbb3b873ee83cb258ce605a97191b66d3ec059
Aympab <aympab@gmail.com>
2022-04-24 19:29:29 +0200

    added bevy raycast package

--- src/bin.rs
+++ src/bin.rs
@@ -4,29 +4,41 @@ mod utils;
 use bevy::prelude::*;
 // use bevy_utils::resources::BevyCamera;
 // use bevy_utils::resources::BevyLight;
-// use bevy_utils::raytracing::compute_rt;
+use bevy_utils::raytracing::compute_rt;
 use bevy_utils::resources::Materials;
 use bevy_utils::resources::Meshes;
-// use bevy_utils::spawns::*;
-use raytracing::run_lib;
+use bevy_utils::spawns::*;
+use bevy_mod_raycast::DefaultRaycastingPlugin;
+// use bevy_mod_raycast::{
+//     DefaultPluginState, DefaultRaycastingPlugin, Intersection, RayCastMesh, RayCastSource,
+// };
+
+
+/*
+Mark our generic `RayCastMesh`s and `RayCastSource`s as part
+of the same "RayCastSet". This plugin uses generics
+to distinguish between groups of raycasters.
+*/
+struct MyRaycastSet;
 
 fn main() {
-    run_lib();
-    // App::new()
-    //     .insert_resource(WindowDescriptor {
-    //         title: "Raytracing app".to_string(),
-    //         width: 500.0,
-    //         height: 300.0,
-    //         ..Default::default()
-    //     })
-    //     .add_plugins(DefaultPlugins)
-    //     .add_startup_system(setup)
-    //     .add_startup_stage("camera_spawn_stage", SystemStage::single(camera_spawn))
-    //     .add_startup_stage("light_spawn_stage", SystemStage::single(light_spawn))
-    //     .add_startup_stage("plane_spawn_stage", SystemStage::single(plane_spawn))
-    //     .add_startup_stage("sphere_spawn_stage", SystemStage::single(sphere_spawn))
-    //     .add_system(compute_rt)
-    //     .run();
+    // run_lib();
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Raytracing app".to_string(),
+            width: 500.0,
+            height: 300.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(DefaultRaycastingPlugin::<MyRaycastSet>::default())
+        .add_startup_system(_setup)
+        .add_startup_stage("camera_spawn_stage", SystemStage::single(camera_spawn))
+        .add_startup_stage("light_spawn_stage", SystemStage::single(light_spawn))
+        .add_startup_stage("plane_spawn_stage", SystemStage::single(plane_spawn))
+        .add_startup_stage("sphere_spawn_stage", SystemStage::single(sphere_spawn))
+        .add_system(compute_rt)
+        .run();
 }
 /// set up a simple 3D scene
 fn _setup(
